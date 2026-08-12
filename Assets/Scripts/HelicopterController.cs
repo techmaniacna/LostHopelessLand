b035365ec4b54ab5b08c84ad9f0e80bd7011115c
techmaniacna <techmaniac.na@gmail.com>
2026-08-12 14:13:52 +0000

    Add HelicopterController script for extraction and escape mechanics

--- Assets/Scripts/HelicopterController.cs
+++ Assets/Scripts/HelicopterController.cs
@@ -2,26 +2,14 @@ using UnityEngine;
 
 public class HelicopterController : MonoBehaviour
 {
-    [Header("Flight Parameters")]
-    public float flightSpeed = 15f;
-    public float rotationSpeed = 45f;
-    public bool isPiloted = false;
+    [Header("Extraction Settings")]
+    public string extractionZone = "Extraction Point Alpha";
+    public float countdownTimer = 30f;
+    public bool isExtractionActive = false;
 
-    void Update()
+    public void StartExtractionCountdown()
     {
-        if (isPiloted)
-        {
-            HandleFlight();
-        }
-    }
-
-    void HandleFlight()
-    {
-        // Simple vertical lift and forward flight mechanics for mobile/touch input
-        float moveForward = Input.GetAxis("Vertical");
-        float turn = Input.GetAxis("Horizontal");
-
-        transform.Translate(Vector3.forward * moveForward * flightSpeed * Time.deltaTime);
-        transform.Rotate(Vector3.up * turn * rotationSpeed * Time.deltaTime);
+        isExtractionActive = true;
+        Debug.Log("Extraction chopper incoming at " + extractionZone + ". Countdown started: " + countdownTimer + "s");
     }
 }
