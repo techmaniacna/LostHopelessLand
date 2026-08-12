1054accd28f5e50eb6d1dc3b206b9854ef3cfcbe
techmaniacna <techmaniac.na@gmail.com>
2026-08-12 14:19:46 +0000

    Add WeaponController script for shooting and ammo mechanics

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -7,25 +7,17 @@ public class WeaponController : MonoBehaviour
     public int currentAmmo = 30;
     public int maxMagazine = 30;
     public float fireRate = 0.15f;
-    private float nextFireTime = 0f;
 
     public void Shoot()
     {
-        if (Time.time >= nextFireTime && currentAmmo > 0)
+        if (currentAmmo > 0)
         {
-            nextFireTime = Time.time + fireRate;
             currentAmmo--;
-            Debug.Log(weaponName + " fired! Ammo remaining: " + currentAmmo + "/" + maxMagazine);
+            Debug.Log(weaponName + " fired! Ammo remaining: " + currentAmmo);
         }
-        else if (currentAmmo <= 0)
+        else
         {
             Debug.Log("Out of ammo! Reload required.");
         }
-    }
-
-    public void Reload()
-    {
-        currentAmmo = maxMagazine;
-        Debug.Log(weaponName + " reloaded. Ammo full: " + currentAmmo);
     }
 }
