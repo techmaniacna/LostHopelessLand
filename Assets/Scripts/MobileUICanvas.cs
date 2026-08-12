75412101ae4efe8b4525b85b757842aff6dc1005
techmaniacna <techmaniac.na@gmail.com>
2026-08-12 13:04:09 +0000

    Add MobileUICanvas script for touch interface

---
+++ Assets/Scripts/MobileUICanvas.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileUICanvas : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Mobile UI Canvas Initialized: Virtual joystick and action buttons ready.");
+    }
+}
